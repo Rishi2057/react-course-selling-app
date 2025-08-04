f83f9266d663b2ec7f0553da452bb0484c52993c
Rishi2057 <rishisankar8@gmail.com>
2025-08-04 23:22:53 +0530

    Update MobileNave.jsx

--- src/components/MobileNave.jsx
+++ src/components/MobileNave.jsx
@@ -8,7 +8,7 @@ function MobileNave() {
              <section className="flex w-full md:hidden fixed z-50 bottom-0 min-h-auto justify-between  bg-white shadow-2xl">
              
         
-                <div className="w-full">
+                <div className="ps-5 w-full">
                   <ul className="flex justify-evenly items-center gap-10 text-slate-500 hover:text-slate-900 p-7 me-20">
                     <a href="#home"><li className="text-sm md:text-slate-500 hover:text-slate-900 md:text-xl font-extrabold">HOME</li></a>
                     <a href="#about"><li className="text-sm md:text-slate-500 hover:text-slate-900 md:text-xl font-extrabold">ABOUT</li></a>
